c80cff842ebaaf2d195db571f68a9c4bc48da25f
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-10 06:05:06 -0700

    Printed the size of various types on the computer it was compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry point.
  *
- * Return - Always 0 (Success)
+ * Return:zero if no error, Non-zero value if errors.
  */
 int main(void)
 {
