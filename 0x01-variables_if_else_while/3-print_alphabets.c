fc8e9b787ebde6f1d308556b61e96faebb1b6348
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-12 13:27:13 -0700

    Printed lower case and upper case alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /*
- * main - Entry point
+ * main:Entry point
  *
- * Description: a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line
  *
  * Return:Always 0 (Success)
  */
