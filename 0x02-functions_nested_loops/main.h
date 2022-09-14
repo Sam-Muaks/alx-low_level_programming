9742ab4ab13190411c6b9513d1ef060effcd2e1b
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-14 08:07:47 -0700

    updated header files, included _putchar file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 /**
  * A program that prints _putchar, followed by a new line
 */
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,3 +18,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+
+#endif /* MAIN_H*/
