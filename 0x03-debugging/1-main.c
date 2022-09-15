73c2b6e0fd93582aff27507e47dbb5058cf8579f
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-15 08:29:39 -0700

    Commented out section of code causing infinite loop usin block comments

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -27,15 +27,15 @@ int main(void)
 			        i = 0;
 
 
-
-				       // while (i < 10)
-
-						        {
-
-								                putchar(i);
-
-										        }
-
+				/**
+				 *       while (i < 10)
+				 *
+				 *		        {
+				 *
+				 *				                putchar(i);
+				 *
+				 *						        }
+				 */
 
 
 					        printf("Infinite loop avoided! \\o/\n");
