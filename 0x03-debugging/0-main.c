25392589026ca4f781d70c0cc2628fc5d80980fa
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-15 04:46:54 -0700

    Testing function 0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
+ * @i: integer that holds value checked
+ * Description: checks if number is positive or negative
+ *
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
