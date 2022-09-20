54d5531ff0924059d8f39af60100ed4e20f863e8
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-20 23:29:12 +0100

    Printed string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - function that prints in reerse
+ *
+ * @s: string to input print
+ *
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them";
+	print_rev(str);
+}
