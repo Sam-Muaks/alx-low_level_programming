b63a0b820036ec49d99309f69b0a8ab2d2bd2271
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-20 21:43:42 +0100

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a character string
+ *
+ * @s: string input
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s);
+{
+	char name[6] = "Mohri";
+	int length = strlen(name);
+}
