87a36b6aab340bd3bd10e51f3eb6490213b56f9b
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-20 23:01:11 +0100

    Function that prints string to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _puts - prints a string to stdout
- * 
+ * @str: parameter to print
  * Return: Always 0
  */
-void _puts(char *str);
+void _puts(char *str)
 {
 	char Mystr[] = "Use code";
 
