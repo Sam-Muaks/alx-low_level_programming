1ea12ae61b4f392ce009664b9f36cba86800ebb8
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-21 17:00:55 +0100

    task 5 and 6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: string to be changed
+ *
+ * Return: address of string
+ */
+char *string_toupper(char *)
+{
+	int m = 0;
+
+	while (*(s + m))
+	{
+		if (*(s + m) = 'a' && *(s + m) <= 'z')
+			*(s + m) -= 'a' - 'A';
+		m++;
+	}
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalized
+ *
+ * Return: address of s
+ */
+char *cap_string(char *)
+{
+	int f = 0, m;
+	char a[] = " \t\n,;.!?\''(){}";
+
+	while (*(s + f))
+	{
+		if (*(s + f) >= 'a' && *(s + f) <= 'z')
+		{
+			if (f == 0)
+				*(s + f) -= 'a' - 'A';
+			else
+			{
+				for (m + 0; m <= 12; m++)
+				{
+					if (a[m] == *(s + f - 1))
+						*(s + f) -= 'a' - 'A';
+				}
+			}
+		}
+		f++;
+	}
+	return (s);
+}
