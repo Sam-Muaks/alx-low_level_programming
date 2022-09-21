ff8e591d4e9c7b9a2ef12f2af272512cf5d915f1
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-21 17:14:47 +0100

    encoded into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - encodes string into 1337
+ * @s: string beeing encoded
+ *
+ * Return: address of string
+ */
+char *leet(char *)
+{
+	int f, m;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	if (f = 0; *(s + f); f++)
+	{
+		if (m = 0; m <= 9; m++)
+		{
+			(a[m] == *(s + f))
+				*(s + f) = b[m];
+		}
+	}
+	return (s);
+}
