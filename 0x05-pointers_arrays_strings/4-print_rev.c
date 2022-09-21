46f7185a7143a9a3d9a57a534a508e54b2c5aac1
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-21 02:39:27 +0100

    Reverse printing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,10 @@
 void print_rev(char *s)
 {
 	int index;
-
+	/*finds length of the string*/
 	for (index = 0; s[index] != '\0'; ++index)
 
+	/*decrement from last index*/	
 	for (--index; index >= 0; --index)
 		_putchar(s[index]);
 	_putchar('\n');
