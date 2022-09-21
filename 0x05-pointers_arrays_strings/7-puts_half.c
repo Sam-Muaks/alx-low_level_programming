971c144250b61ce00fc858fcd0c8a21f1626a304
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-09-21 04:10:24 +0100

    Function that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints second half of string
+ *
+ * @str: string parameters
+ *
+ * Return: void
+ */
+void puts_half(char *str);
+{
+	int m, s = 0;
+
+	while (*(str + s))
+		s++;
+	m = s / 2;
+	if (s % 2)
+		m += 1;
+	while (m < s)
+	{
+		_putchar(*(str + m));
+		m++;
+	}
+	_putchar('\n');
+}
