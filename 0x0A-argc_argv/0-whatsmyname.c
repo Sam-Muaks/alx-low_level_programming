bf680cbe852a9dbbf750f3edba163d557fec01a1
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-10-01 00:06:46 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point of the program
+ *
+ * Description: prints the name of the program
+ *
+ * @argc: number of arguments passed through the program
+ * @argv: pointer to arrays of the arguments passed
+ * Return: Always 0 (success)
+ */
+int main(int argc _attribute_((unused), char *argv[])
+{
+printf("%s\n". argv[0];
+		return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 int _putchar(char *s);
+int main(int argc __attribute__((unused)), char *argv[]);
 
 #endif
