66c5d712056294cb7cff18450c6e076dc79e64e4
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-10-01 00:20:43 +0100

    corrected arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  * @argv: pointer to arrays of the arguments passed
  * Return: Always 0 (success)
  */
-int main(int argc _attribute_((unused), char *argv[])
+int main(int argc, char *argv[])
 {
 printf("%s\n". argv[0];
 		return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,6 @@
 #include <stdlib.h>
 
 int _putchar(char *s);
-int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
