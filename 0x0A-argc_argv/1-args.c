9dbd60ed7a3edb7d043db9ea6d30b0c2858d862b
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-10-01 10:01:57 +0100

    print number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,11 +7,21 @@
  * Description: prints the number of arguments passed to it
  *
  * @argc: returns counts supplied to the program
+ * @argv: pointer to array of arguments
  *
  * Return: Always 0 (success)
  */
-int main(int argc)
+int main(int argc, char *argv [])
 {
-	printf("%d\n", argc);
+	int m;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (m = 0; *argv; m++, argv++)
+			;
+		printf("%d\n", m - 1):
+	}
 	return (0);
 }
