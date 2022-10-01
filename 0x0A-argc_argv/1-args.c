e8fc66fc4d43a74ff41ba93bbdd63f924c00067f
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-10-01 09:45:34 +0100

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point to the program
+ *
+ * Description: prints the number of arguments passed to it
+ *
+ * @argc: returns counts supplied to the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
