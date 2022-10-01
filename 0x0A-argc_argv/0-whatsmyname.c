7fc76ee45468fea1eae242f0f55dc5ab4c19d909
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-10-01 09:27:50 +0100

    corrected errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,13 +6,12 @@
  *
  * Description: prints the name of the program
  *
- * @__attribute__: ignores unused attributes
  * @argc: number of arguments passed through the program
  * @argv: pointer to arrays of the arguments passed
  * Return: Always 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc *0]);
+	printf("%s\n", argv[argc * 0]);
 	return (0);
 }
