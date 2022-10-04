a3de2540cca99b7cf358b21d862fc69759840d54
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-10-04 05:49:52 +0100

    functions complete

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - apends src to dest
+ *
+ * @dest: string appended to
+ * @src: string appended from
+ * @n: number of bytes to append
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int m, s;
+
+	m = s = 0;
+	while (*(dest + m))
+		m++;
+	while (s < n && *(src + s))
+	{
+		*(dest + m) = *(src + s);
+		m++;
+		s++;
+	}
+	if (s < n)
+		*(dest + m) = *(src + s);
+	return (dest);
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurrence in the string s of any of the bytes
+ * in the string accept
+ *
+ * @s: pointer to string beeing searched
+ * @accept: pointer that contains the string beeing used to evaluate s
+ *
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int m;
+
+	while (*s != 0)
+	{
+		for (m = 0; accept[m] != 0; m++)
+		{
+			if (*s == accept[m])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (0);
+
+}
