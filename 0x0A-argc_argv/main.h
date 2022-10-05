864cbd705e0cbfbbd704c968119dc9fc14ea1536
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-10-05 06:03:14 +0100

    declared functions

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char c);
 int coinConverter(int i);
+void print_number(int);
+int _atoi(char *);
+int isnumber(char *);
+int subtract_largest_coin(int);
 
-#endif
+#endif /* MAIN_H */
