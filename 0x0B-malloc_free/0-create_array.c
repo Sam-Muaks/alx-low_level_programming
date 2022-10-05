f39462501b20eb7abceeba0c6fc8aac7cb6eaa02
Mohri Samuel Muakpo <mohrimuakpo@gmail.com>
2022-10-05 04:47:14 +0100

    fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	char *s = (char *)malloc(size * sizeof(char))
+	char *s=(char*)malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 		s[i] = c;
