2bff291d3d320a73603127d1b0962ec93977e9ee
Sam-Muaks <mohrimuakpo@gmail.com>
2022-11-02 11:42:43 +0100

    1 corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1);
-	return (-1);
+		return (-1);
 	if (text_content)
 	{
 		for (i = 0; *(text_content + i); i++)
